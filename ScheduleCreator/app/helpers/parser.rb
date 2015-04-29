5728b9e305ed98939ef4a0ee1875e795279cc28d
Shikib <mehrishikib@gmail.com>
2015-04-28 23:03:11 -0700

    fixed OpenSSL configuration problem by bypassing SSL verification

--- ScheduleCreator/app/helpers/parser.rb
+++ ScheduleCreator/app/helpers/parser.rb
@@ -2,10 +2,11 @@ module Parser
   require 'open-uri'
 
   def parse_subject
-    uri = open("https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=1&dept=CPSC")
+    uri  = open("https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=1&dept=CPSC", {ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE})
     doc = Nokogiri::HTML(uri)
     sub = Subject.new
     sub.description = doc.at_css("p")
+    sub.save
   end
 
 end
--- ScheduleCreator/db/schema.rb
+++ ScheduleCreator/db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150429045917) do
+
+  create_table "subjects", force: true do |t|
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
