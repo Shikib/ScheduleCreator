5f51153a63cfd4982d151e2bfeb864b79bddc5fe
Shikib <mehrishikib@gmail.com>
2015-04-28 23:21:57 -0700

    Removed \r from parsed description.

--- ScheduleCreator/app/helpers/parser.rb
+++ ScheduleCreator/app/helpers/parser.rb
@@ -5,7 +5,7 @@ module Parser
     doc = Nokogiri::HTML(open("https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=1&dept=CPSC", {ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE}))
 
     description = doc.at_css("p").to_s
-    description = description.delete("<p>").delete("</p>").delete("\n")
+    description = description.delete("<p>").delete("</p>").delete("\n").delete("\r")
 
     subject = Subject.new
     subject.description = description
