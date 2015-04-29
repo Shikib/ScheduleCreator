260950bb3ce813ffce4a8127f1fcb2cf87021d47
Shikib <mehrishikib@gmail.com>
2015-04-28 23:16:57 -0700

    Subject description parsing works properly now.

--- ScheduleCreator/app/helpers/parser.rb
+++ ScheduleCreator/app/helpers/parser.rb
@@ -2,11 +2,14 @@ module Parser
   require 'open-uri'
 
   def parse_subject
-    uri  = open("https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=1&dept=CPSC", {ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE})
-    doc = Nokogiri::HTML(uri)
-    sub = Subject.new
-    sub.description = doc.at_css("p")
-    sub.save
+    doc = Nokogiri::HTML(open("https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=1&dept=CPSC", {ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE}))
+
+    description = doc.at_css("p").to_s
+    description = description.delete("<p>").delete("</p>").delete("\n")
+
+    subject = Subject.new
+    subject.description = description
+    subject.save
   end
 
 end
